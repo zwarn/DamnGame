f03642e4a99392c5684ec3956bfae3273267d5d2
zwarn <michael.budahn@gmail.com>
2021-03-07 17:54:08 +0100

    selection

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private Input _input = new Input();
+    private Selector _selector;
+
+    private void Awake()
+    {
+        _input.Play.Throw.performed += context => OnInteract();
+        _selector = GetComponent<Selector>();
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _input.Disable();
+    }
+
+    private void OnInteract()
+    {
+        Interactable interactable = _selector.Selected();
+    }
+}
---
+++ Assets/Scripts/Selector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Selector : MonoBehaviour
+{
+    private readonly List<Interactable> _interactables = new List<Interactable>();
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var interactable = other.gameObject.GetComponent<Interactable>();
+        if (interactable != null)
+        {
+            _interactables.Add(interactable);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        var interactable = other.gameObject.GetComponent<Interactable>();
+        if (interactable != null)
+        {
+            _interactables.Add(interactable);
+        }
+    }
+
+    private Interactable closest()
+    {
+        if (_interactables.Count == 0)
+        {
+            return null;
+        }
+
+        return _interactables.OrderBy(interactable =>
+            (interactable.GetGameObject().transform.position - transform.position).magnitude).First();
+    }
+
+    public Interactable Selected()
+    {
+        return closest();
+    }
+}
