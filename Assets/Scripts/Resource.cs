e28e4b85b7def88ded425c10c25eeeff6c59c18a
zwarn <michael.budahn@gmail.com>
2021-03-08 16:44:34 +0100

    item source

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Resource : MonoBehaviour, Interactable
+{
+    public GameObject resource;
+
+    public GameObject GetGameObject()
+    {
+        return gameObject;
+    }
+
+    public void Interact(PlayerController playerController)
+    {
+        if (!playerController.HasItem())
+        {
+            Item item = Instantiate(resource).GetComponent<Item>();
+            playerController.PickupItem(item);
+        }
+    }
+}
