372002f91ae58567fb78897b3fa894e8215bd27f
AntiElitz <berghoff@gmx.de>
2021-03-10 20:27:43 +0100

    movement controller mit custon acc and dec in ground/air

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,38 +2,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    [SerializeField]
+    private float accelerationHorizontalGround;
+    [SerializeField]
+    private float decelerationHorizontalGround;
+    [SerializeField]
+    private float accelerationHorizontalAir;
+    [SerializeField]
+    private float decelerationHorizontalAir;
+    [SerializeField]
+    private float maxMovementSpeedHorizontal;
+    [SerializeField]
+    private float peakJumpReducedGravityMultiplier;
+    [SerializeField]
+    private float peakJumpReducedGravityAttackSpeed;
+    [SerializeField]
+    private float jumpCornerCorrectionInTiles;
+    [SerializeField]
+    private float dashForce;
+    [SerializeField]
+    private float dashCornerCorrectionInTiles;
+    [SerializeField]
+    private float jumpForce;
+    
+    
+    
+    
     private Input _input;
+    private float _xInput;
     private Rigidbody2D _rigidbody;
     private Item _currentItem;
     private bool _isWorking;
-    public int isGrounded = 0;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+    private int _isGrounded = 0;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
 
     public Collider2D bottomEdgeCollider2D;
-    public Animator animator;
+    public GameObject animationGameObject;
     public Selector selector;
-    public float moveSpeed;
-    public float jumpPower;
     public float throwPower;
-    
+
+    private Vector2 _lastSpeed;
+    public Vector2 speed;
+    public Vector2 acceleration;
+
+    private Animator _animator;
     
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.otherCollider == bottomEdgeCollider2D) {
-            isGrounded++;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
-            animator.SetInteger("isGrounded", isGrounded);
-            Debug.Log(isGrounded);
+            _isGrounded++;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+            _animator.SetInteger("isGrounded", _isGrounded);
+            Debug.Log(_isGrounded);
         }
     }
     
     private void OnCollisionExit2D(Collision2D col)
     {
         if (col.otherCollider == bottomEdgeCollider2D) {
-            isGrounded--;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
-            animator.SetInteger("isGrounded", isGrounded);
-            Debug.Log(isGrounded);
+            _isGrounded--;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+            _animator.SetInteger("isGrounded", _isGrounded);
+            Debug.Log(_isGrounded);
         }
     }
 
@@ -41,6 +73,7 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        _animator = animationGameObject.GetComponent<Animator> ();
         _rigidbody = GetComponent<Rigidbody2D>();
         _input = new Input();
         _input.Play.Throw.performed += context => Throw();
@@ -63,31 +96,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        var vertical = _input.Play.Movement.ReadValue<float>();
-        if (Math.Abs(vertical) > 0.5)
-        {
-            // player holds left or right
-
-            // normalize controller input
-            if (vertical < 0)
-            {
-                vertical = -1;
-            }
-            else
-            {
-                vertical = 1;
-            }
-
-            transform.localScale = new Vector3(-vertical, 1, 1); // flip player towards pressed direction 
-            _rigidbody.AddForce(Vector2.right * (vertical * moveSpeed * Time.deltaTime));
-            animator.SetFloat("Speed", 1);
-        }
-        else
-        {
-            // no input or controller input too low
-            animator.SetFloat("Speed", 0);
-        }
-
+        CheckInput();
+        SetAnimator();
 
         var interactable = selector.Selected();
         if (interactable != null && interactable is Workable workable && _isWorking && !HasItem())
@@ -95,7 +105,13 @@ public class PlayerController : MonoBehaviour
             workable.Work(this);
         }
     }
-
+    
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+        MyDebug();
+    }
+    
     void Throw()
     {
         if (HasItem())
@@ -108,7 +124,7 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        _rigidbody.AddForce(Vector2.up * (jumpPower));
+        _rigidbody.AddForce(Vector2.up * (jumpForce));
     }
 
     void Interact()
@@ -159,5 +175,102 @@ public class PlayerController : MonoBehaviour
         item.GetComponent<Rigidbody2D>().isKinematic = false;
         item.GetComponent<Collider2D>().enabled = true;
         return item;
+    }
+    
+    private void CheckInput()
+    {
+        // get X Input
+        var vertical = _input.Play.Movement.ReadValue<float>();
+        
+        // player holds left or right?
+        if (Math.Abs(vertical) > 0.5)
+        {
+            // normalize controller input
+            if (vertical < 0)
+            {
+                _xInput = -1;
+            }
+            else
+            {
+                _xInput = 1;
+            }
+            
+            // flip player towards pressed direction 
+            transform.localScale = new Vector3(-_xInput, 1, 1); 
+        }
+        else
+        {
+            _xInput = 0;
+        }
+    }
+
+    private void SetAnimator()
+    {
+        _animator.SetFloat("hasXInput", Math.Abs(_xInput));
+    }
+
+    private void ApplyMovement()
+    {
+        if (_isGrounded > 0) // player is on ground
+        {
+            if (_xInput == 0)
+            {
+                // player stopping on ground // * Time.deltaTime
+                // * (1 - (1 / (decelerationSpeedHorizontal * maxMovementSpeedHorizontal)))
+                _rigidbody.AddForce(Vector2.right * ( -_rigidbody.velocity.x * Time.deltaTime * 3600 * (1-1/(decelerationHorizontalGround+1))));
+            }
+            else
+            {
+                // player accelerating on ground
+                var maxFactor = Math.Abs((_xInput * maxMovementSpeedHorizontal - _rigidbody.velocity.x)) / maxMovementSpeedHorizontal;
+                var force = maxFactor * accelerationHorizontalGround * maxMovementSpeedHorizontal;
+                _rigidbody.AddForce(Vector2.right * (_xInput * force * Time.deltaTime));
+            }
+        }
+        else // player is in air
+        {
+            if (_xInput == 0)
+            {
+                // player stopping on air // * Time.deltaTime
+                // * (1 - (1 / (decelerationSpeedHorizontal * maxMovementSpeedHorizontal)))
+                _rigidbody.AddForce(Vector2.right * ( -_rigidbody.velocity.x * Time.deltaTime * 3600 * (1-1/(decelerationHorizontalAir+1))));
+            }
+            else
+            {
+                // player accelerating on air
+                var maxFactor = Math.Abs((_xInput * maxMovementSpeedHorizontal - _rigidbody.velocity.x)) / maxMovementSpeedHorizontal;
+                var force = maxFactor * accelerationHorizontalAir * maxMovementSpeedHorizontal;
+                _rigidbody.AddForce(Vector2.right * (_xInput * force * Time.deltaTime));
+            }
+        }
+    }
+
+
+    private void MyDebug()
+    {
+        _lastSpeed = speed;
+        speed = _rigidbody.velocity;
+        if (Math.Abs(speed.x) < 0.01)
+        {
+            speed.x = 0;
+        }
+        else
+        {
+            Debug.Log(speed.x);
+        }
+
+        if (Math.Abs(speed.y) < 0.01)
+        {
+            speed.y = 0;
+        }
+        acceleration = speed - _lastSpeed;
+        if (Math.Abs(acceleration.x) < 0.01)
+        {
+            acceleration.x = 0;
+        }
+        if (Math.Abs(acceleration.y) < 0.01)
+        {
+            acceleration.y = 0;
+        }
     }
 }
