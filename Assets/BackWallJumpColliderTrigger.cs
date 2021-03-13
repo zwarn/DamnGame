ab4b5deee341e22e172b490913917317f4ea024f
AntiElitz <berghoff@gmx.de>
2021-03-13 06:22:18 +0100

    scene changes

---
+++ Assets/BackWallJumpColliderTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BackWallJumpColliderTrigger : MonoBehaviour
+{
+    public int isCollidingWallJumpBack;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+    public float isCollidingWallJumpBackFacingDirection;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        isCollidingWallJumpBack++;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+        isCollidingWallJumpBackFacingDirection = transform.parent.localScale.x;
+    }
+    
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        isCollidingWallJumpBack--;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+        isCollidingWallJumpBackFacingDirection = 0;
+    }
+}
--- Assets/FrontWallJumpColliderTrigger.cs
+++ Assets/FrontWallJumpColliderTrigger.cs
@@ -3,13 +3,16 @@
 public class FrontWallJumpColliderTrigger : MonoBehaviour
 {
     public int isCollidingWallJumpFront;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+    public float isCollidingWallJumpFrontFacingDirection;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
     private void OnTriggerEnter2D(Collider2D col)
     {
         isCollidingWallJumpFront++;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+        isCollidingWallJumpFrontFacingDirection = transform.parent.localScale.x;
     }
     
     private void OnTriggerExit2D(Collider2D col)
     {
         isCollidingWallJumpFront--;  // can't use bool as OnCollisionEnter2D/Exit2d order is undeterministic
+        isCollidingWallJumpFrontFacingDirection = 0;
     }
 }
